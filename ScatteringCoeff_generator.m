831f8144498f049ad4ffff5e0c323c79ccd6a718
Ruchi Chauhan <ruchi.chauhan028@gmail.com>
2021-04-26 04:28:14 +0530

    Initial commit for SC

---
+++ ScatteringCoeff_generator.m
@@ -0,0 +1,22 @@
+%%% Obtain Scaterring coefficients of the input images from a scattering network
+%%% Used toolbox
+
+data_path = 'US_testImages/';
+
+tfiles=dir([data_path '*.mat']);
+
+for k=1:numel(tfiles)
+    if (mod(k, 50)) == 0
+        k
+    end
+    img = imread([data_path tfiles(k).name]);
+    filename = tfiles(k).name;
+    name = filename(1:end-4);
+    img = imresize(img,[512 512]);
+
+    Wop = wavelet_factory_2d(size(img));
+
+    St = scat(double(img), Wop);
+    S = format_scat(St);
+    save(['sc_bcet/' name], 'S');
+end
